031b241f355c018059cc6a02e8946cb3e42f93b3
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-08-28 11:40:46 -0300

    ✨ Add 2160 problem

---
+++ uri_lista01/2160/prog.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    string name; getline(cin, name);
+
+    name.length() > 80 ? cout << "NO" << endl : cout << "YES" << endl;
+
+    return 0;
+}
