28204b24bacc8c7b6276b4921a89e4588d309c58
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-09-05 00:19:14 -0300

    🐛 Fix tle problem

--- native_types/v10591/prog.cpp
+++ native_types/v10591/prog.cpp
@@ -2,39 +2,34 @@
 
 using namespace std;
 
-bool ishappy (int x) {
-    int y = x;
-
-    do {
-        int p = (int) log10(x);
-        int sum = 0;
-
-        int t = x;
-        for (int i = p; i >= 0; i--) {
-            sum += pow((t / pow(10, i)), 2);
-            t = t - ((t / pow(10, i)) * pow(10, i));
-        }
-
-        x = sum;
-        if (x == y) {
-            return false;
-        }
-    } while (x != 1);
-
-    return true;
-}
-
 int main () {
 
-    int n; cin >> n;
+    int n; scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        int x; cin >> x;
+        int x; scanf(" %d", &x);
 
-        if (ishappy(x)) {
-            cout << "Case #" << i << ": " << x << " is a Happy number." << endl;
+        int y = x, sum = 0, d;
+
+        while (x != 1) {
+            sum = 0;
+
+            while (x != 0) {
+                d = x % 10;
+                x = x / 10;
+                sum += (d * d);
+            }
+            x = sum;
+
+            if (x <= 9) {
+                break;
+            }
+        }
+
+        if (x == 1) {
+            printf("Case #%d: %d is a Happy number.\n", i, y);
         } else {
-            cout << "Case #" << i << ": " << x << " is an Unhappy number." << endl;
+            printf("Case #%d: %d is an Unhappy number.\n", i, y);
         }
     }
 
