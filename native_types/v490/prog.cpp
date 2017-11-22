2311bebc964bfc2d1ee92686a05814edf6b34bc1
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-11-22 21:58:00 -0200

    Add 490 uva problem

---
+++ native_types/v490/prog.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int pos = 0;
+    int longest = 0;
+    string sentences[105];
+    while (getline(cin, sentences[pos])) {
+        if (sentences[pos].size() > longest)
+            longest = sentences[pos].size();
+        ++pos;
+    }
+
+    for (int j = 0; j < longest; ++j)
+    {
+        for (int i = pos - 1; i >= 0; --i)
+        {
+            if (sentences[i].size() > j)
+                cout << sentences[i][j];
+            else
+                cout << ' ';
+        }
+        cout << '\n';
+    }
+}
